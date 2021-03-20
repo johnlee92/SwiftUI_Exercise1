5b00ecac2cca49db1601e27aa5d454550867e4bb
이재현 <jaehyeon.lee@navercorp.com>
2021-03-20 11:40:44 +0900

    Initial Commit

---
+++ ThinkingInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThinkingInSwiftUI
+//
+//  Created by 이재현 on 2021/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThinkingInSwiftUI/ThinkingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingInSwiftUIApp.swift
+//  ThinkingInSwiftUI
+//
+//  Created by 이재현 on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct ThinkingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
